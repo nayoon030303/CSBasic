478f23fe1b3b9969926c9d8dc62fe79a0f9dda7f
Nayoon Kim <nayoon030303@gmail.com>
2021-04-23 15:51:54 +0900

    #7 자료형 검사 예시

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -129,7 +129,22 @@ namespace CSBasic
             output3 += "World";
             output3 = output3 + "?";
             Console.WriteLine(output3);
-        
+
+            int _int = 273;
+            long _long = 3545203904;
+            float _float = 52.123f;
+            double _double = 52.1234;
+            char _char = '귤';
+            string _string = "문자열";
+
+            Console.WriteLine(_int.GetType());
+            Console.WriteLine(_long.GetType());
+            Console.WriteLine(_float.GetType());
+            Console.WriteLine(_double.GetType());
+            Console.WriteLine(_char.GetType());
+            Console.WriteLine(_string.GetType());
+
+
         }
     }
 }
