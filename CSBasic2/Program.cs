986c82f5ca17bf3ab11d1b3f7f0123e3a36d3e46
Nayoon Kim <nayoon030303@gmail.com>
2021-05-04 12:23:36 +0900

    #14  Ternary operator

--- CSBasic2/Program.cs
+++ CSBasic2/Program.cs
@@ -93,6 +93,10 @@ namespace CSBasic2
                     break;
 
             }
+            string input2 = Console.ReadLine();
+            int number = int.Parse(input2);
+            Console.WriteLine((number > 0) ? "자연수" : "자연수 아님");
+            //Console.WriteLine((number > 0) ? a() : b());
         }
     }
 }
