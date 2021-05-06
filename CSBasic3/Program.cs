e64a6485e6e855a462a4cb87151fb462843991fe
Nayoon Kim <nayoon030303@gmail.com>
2021-05-06 10:15:33 +0900

    #18 while

--- CSBasic3/Program.cs
+++ CSBasic3/Program.cs
@@ -13,6 +13,21 @@ namespace CSBasic3
             //Console.WriteLine(intArray[100]); //범위 초과
             //Console.WriteLine(intArray[-1]); //음수 사용불가
 
+
+            int i = 0;
+            int[] intArray2 = { 52, 123, 4, 3, 55, 645, 3 };
+            while (i < intArray2.Length)
+            {
+                Console.WriteLine(i + "번째 출력" + intArray2[i]);
+                i++;
+            }
+
+            string input;
+            do
+            {
+                Console.Write("입력(종료:exit):");
+                input = Console.ReadLine();
+            } while (input != "exit");
         }
     }
 }
