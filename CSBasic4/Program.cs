02bafffdff3a7cb361f90ec94236569f3b085289
s2019s05 <s2019s05@e-mirim.hs.kr>
2021-05-27 09:50:00 +0900

    # 22 클래스 개요, 사용, 생성

--- CSBasic4/Program.cs
+++ CSBasic4/Program.cs
@@ -6,10 +6,61 @@ using System.Threading.Tasks;
 
 namespace CSBasic4
 {
+    class Car
+    {
+        int carNumber;
+        DateTime inTime;
+        DateTime outTime;
+
+        public void SetInTime()
+        {
+            this.inTime = DateTime.Now;
+        }
+
+        public void SetOutTime()
+        {
+            this.outTime = DateTime.Now;
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Car[] cars = new Car[10];
+
+            Random random = new Random();
+            Console.WriteLine(random.Next());
+            Console.WriteLine(random.Next(100));
+            Console.WriteLine(random.Next(1,19));
+            Console.WriteLine(random.NextDouble());
+            Console.WriteLine(random.NextDouble());
+            Console.WriteLine(random.NextDouble());
+            Console.WriteLine(random.NextDouble());
+
+            /*  List<int> list = new List<int>();
+              list.Add(52);
+              list.Add(276);
+              list.Add(33);
+              list.Add(56);
+              foreach(var item in list)
+              {
+                  Console.WriteLine(item);
+              }*/
+
+            List<int> list = new List<int>() { 52, 33, 114, 55 };
+            foreach(var item in list)
+            {
+                Console.WriteLine("Count:" + list.Count + "\titem:" + item);
+               // list.Remove(item);
+            }
+
+            Console.WriteLine(Math.Abs(-523));
+            Console.WriteLine(Math.Ceiling(52.123));
+            Console.WriteLine(Math.Floor(52.123));
+            Console.WriteLine(Math.Round(54.34));
+            Console.WriteLine(Math.Max(33, 124));
+            Console.WriteLine(Math.Min(33, 124));
         }
     }
 }
