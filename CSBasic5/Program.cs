b76cd46a16364254edc77413d5461965b5987693
nayoon <s2019s05@e-mirim.hs.kr>
2021-06-22 12:03:13 +0900

    # 25 클래스 기본 사용

--- CSBasic5/Program.cs
+++ CSBasic5/Program.cs
@@ -10,6 +10,40 @@ namespace CSBasic5
     {
         static void Main(string[] args)
         {
+
+            Car car = new Car();
+            car.SetInTime();
+            car.SetOutTime();
+
+            Random random = new Random();
+
+            // min 이상 max 이하
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.Next(10, 100));
+
+            Console.WriteLine(random.NextDouble());
+
+            Console.WriteLine(random.NextDouble() * 10);
+            Console.WriteLine(random.NextDouble() * 10);
+            Console.WriteLine(random.NextDouble() * 10);
+            Console.WriteLine(random.NextDouble() * 10);
+
+            Console.WriteLine(random.Next(20));
+
+            // 1 이상 20 이하의 값
+            Console.WriteLine(random.Next(20) + 1);
+            Console.WriteLine(random.Next(1, 21));
+
+            int[] intArray = new int[10];
+            long[] longArray = new long[10];
+            string[] stringArray = new string[10];
+
+            List<int> list = new List<int>();
+
+
         }
     }
 }
