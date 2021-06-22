be28c30811c8f59a88d8a9298edccf128f63debe
nayoon <s2019s05@e-mirim.hs.kr>
2021-06-22 12:21:08 +0900

    # 29 메서드 기본

--- CSBasic5/Program.cs
+++ CSBasic5/Program.cs
@@ -8,43 +8,35 @@ namespace CSBasic5
 {
     class Program
     {
+        class MethodExample
+        {
+            public int Power(int x)
+            {
+                return x * x;
+            }
+
+            public int Multi(int x, int y)
+            {
+                return x * y;
+            }
+
+            public void Print()
+            {
+                Console.WriteLine("Print() 메서드 호출됨!");
+            }
+            
+        }
+
+
         static void Main(string[] args)
         {
 
-            Car car = new Car();
-            car.SetInTime();
-            car.SetOutTime();
-
-            Random random = new Random();
-
-            // min 이상 max 이하
-            Console.WriteLine(random.Next(10, 100));
-            Console.WriteLine(random.Next(10, 100));
-            Console.WriteLine(random.Next(10, 100));
-            Console.WriteLine(random.Next(10, 100));
-            Console.WriteLine(random.Next(10, 100));
-
-            Console.WriteLine(random.NextDouble());
-
-            Console.WriteLine(random.NextDouble() * 10);
-            Console.WriteLine(random.NextDouble() * 10);
-            Console.WriteLine(random.NextDouble() * 10);
-            Console.WriteLine(random.NextDouble() * 10);
-
-            Console.WriteLine(random.Next(20));
-
-            // 1 이상 20 이하의 값
-            Console.WriteLine(random.Next(20) + 1);
-            Console.WriteLine(random.Next(1, 21));
-
-            int[] intArray = new int[10];
-            long[] longArray = new long[10];
-            string[] stringArray = new string[10];
-
-            List<int> list = new List<int>();
-
-
-
+            MethodExample me = new MethodExample();
+            Console.WriteLine(me.Power(10));
+            Console.WriteLine(me.Power(20));
+            Console.WriteLine(me.Multi(20,10));
+            Console.WriteLine(me.Multi(3,10));
+            me.Print();
         }
     }
 }
