6339700fea33e59bfcf8b84a33df4970faef0de4
nayoon <s2019s05@e-mirim.hs.kr>
2021-07-01 10:08:58 +0900

    #30 생성자, 정적 쟁성자

--- CSBasic5/Program.cs
+++ CSBasic5/Program.cs
@@ -9,6 +9,43 @@ namespace CSBasic5
    
     class Program
     {
+        class Product
+        {
+            public static int counter = 0;
+            public int id;
+            public string name;
+            public int price;
+
+            public override string ToString()
+            {
+                return this.id + ":" + this.name;
+            }
+
+            public Product(string name, int price)
+            {
+                //Product.counter = counter + 1;
+                counter++;
+                this.id = counter;
+                this.name = name;
+                this.price = price;
+            }
+
+
+        }
+
+        class Sample
+        {
+            public static int value;
+
+            //자동으로 생성되는 생성자 이기 때문에 매개변수를 넣을 수 없다.
+            //접근제어자, 리턴값이 없다.
+            //클래스변수만 접근 가능(인스턴스 불가능)
+            static Sample()
+            {
+                value = 10;
+                Console.WriteLine("처음으로 로드");
+            }
+        }
 
         int someModifier = 0;
 
@@ -112,6 +149,23 @@ namespace CSBasic5
             Console.WriteLine(MyMath.Abs(-234));
             Console.WriteLine(MyMath.Abs(52.123));
             Console.WriteLine(MyMath.Abs(4678984561211111111));
+
+
+            Product productA = new Product("감자", 2000);
+            Product productB = new Product("고구마", 2500);
+            Product productC= new Product("옥수수", 1500);
+
+            //Console.WriteLine(productA.id + ":" + productA.name);
+            Console.WriteLine(productA);
+            Console.WriteLine(productB);
+            Console.WriteLine(productC);
+
+           
+            Console.WriteLine("첫 번째 위치");
+            Sample sample = new Sample();
+            Console.WriteLine("첫 번째 위치");
+            Console.WriteLine(Sample.value);
+            Console.WriteLine("첫 번째 위치");
         }
     }
 }
