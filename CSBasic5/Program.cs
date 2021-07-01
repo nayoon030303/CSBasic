8055ad83cda2cefee1e188527f3e1cd3e1c1c38b
nayoon <s2019s05@e-mirim.hs.kr>
2021-07-01 09:52:48 +0900

    #30 접근제한자 public, private 예제

--- CSBasic5/Program.cs
+++ CSBasic5/Program.cs
@@ -9,6 +9,9 @@ namespace CSBasic5
    
     class Program
     {
+
+        int someModifier = 0;
+
         public int instanceVariable = 10;
 
         public int instanceMethod() 
@@ -43,6 +46,11 @@ namespace CSBasic5
 
         class Test
         {
+            public void TestMethod()
+            {
+                Program.Main(new string[] { "" });
+            }
+
             //Method Signature, return은 포함 x
             public int Sum(int min, int max)
             {
