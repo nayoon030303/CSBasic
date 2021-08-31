f8989c7b12c4cdfe183e4ec4a06ae573cce2bb5c
nayoon030303 <nayoon030303@gmail.com>
2021-08-31 10:52:07 +0900

    제네릭 예제

--- CsBasic7/Program.cs
+++ CsBasic7/Program.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace CsBasic7
 {
+    
+    class Student
+    {
+        public override string ToString()
+        {
+            return "학생";
+        }
+    }
+
     class Wanted<T>
+        //WHER T:Student
     {
         public T value;
         public Wanted(T value)
@@ -18,6 +28,15 @@ namespace CsBasic7
     {
         static void Main(string[] args)
         {
+            Wanted<string> vs = new Wanted<string>("String");
+            Wanted<int> wi = new Wanted<int>(42);
+            Wanted<string> ws = new Wanted<string>("String입니다.");
+            Wanted<Student> wstudent = new Wanted<Student>(new Student());
+
+            Console.WriteLine(vs);
+            Console.WriteLine(wi);
+            Console.WriteLine(ws);
+            Console.WriteLine(wstudent);
         }
     }
 }
