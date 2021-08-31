f7163a1cf6e161e4ced8d39066e4d931a3efb230
nayoon030303 <nayoon030303@gmail.com>
2021-08-31 12:45:24 +0900

    구조체와 구조체 생성, 클래스와 비교

--- CsBasic7/Program.cs
+++ CsBasic7/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace CsBasic7
 {
+    
     class Products
     {
         private List<string> list = new List<string>();
@@ -45,6 +46,34 @@ namespace CsBasic7
     }
     class Program
     {
+        struct Point
+        {
+            public int x;
+            public int y;
+            public string s;
+            public Point(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+                this.s = null;
+            }
+        }
+
+        static void NextPosition(int x, int y, int vx, int vy, out int rx, out int ry)
+        {
+            rx = x + vx;
+            ry = y + vy;
+        }
+        class PointClass
+        {
+            public int x = 0;
+            public int y = 0;
+            public PointClass(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
         static void Main(string[] args)
         {
             Wanted<string> vs = new Wanted<string>("String");
@@ -61,6 +90,52 @@ namespace CsBasic7
             Random rs = new Random();
             int i = rs.Next(0, 5);
             Console.WriteLine(ps[i]);
+
+            Console.Write("숫자 입력: ");
+            int output;
+            bool result = int.TryParse(Console.ReadLine(), out output);
+            if (result)
+            {
+                Console.WriteLine("변환 성공" + output);
+            }
+            else
+            {
+                Console.WriteLine("변환 실패"+output);
+            }
+
+            int x = 0;
+            int y = 0;
+            int vx = 1;
+            int vy = 1;
+            Console.WriteLine("현재좌료:" + x+","+ y);
+            NextPosition(x, y, vx, vy, out x, out y);
+            Console.WriteLine("다음 좌료" + x + "," + y);
+
+            Point point;
+            point.x = 10;
+            point.y = 10;
+
+            Point point2 = new Point(10, 10);
+            Point point3 = new Point();//0,0으로 초기화 
+
+
+            //모두 스택에있다
+            Point p = new Point(10, 20);
+            Point p2 = new Point();
+            p2.x = 100;
+            p2.y = 200;
+            Console.WriteLine(p.x + "," + p.y);
+            Console.WriteLine(p2.x + "," + p2.y);
+
+
+            PointClass pc = new PointClass(10, 20);
+            PointClass pc2 = pc;
+
+            pc2.x = 100;
+            pc2.y = 200;
+            Console.WriteLine(pc.x + "," + pc.y);
+            Console.WriteLine(pc2.x + "," + pc2.y);
+
         }
     }
 }
