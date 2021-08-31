d9f56795752dbb23b658f008148828bb4a548408
nayoon030303 <nayoon030303@gmail.com>
2021-08-31 10:44:34 +0900

    초기 커밋.

---
+++ CsBasic7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsBasic7
+{
+    class Wanted<T>
+    {
+        public T value;
+        public Wanted(T value)
+        {
+            this.value = value;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
